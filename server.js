beccd56622c3cd9d721aa5ddc53d24f328f63028
disha <dishagsuvarna@gmail.com>
2026-06-12 12:30:56 +0530

    Fix Vercel serverless deployment

--- server.js
+++ server.js
@@ -5,10 +5,15 @@ const path = require('path');
 const todoModel = require('./todoModel');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+module.exports = app;
 
-app.use(cors());
-app.use(bodyParser.json());
+if (process.env.NODE_ENV !== 'production') {
+  const PORT = process.env.PORT || 3000;
+
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+}
 
 app.use(express.static(path.join(__dirname, 'public')));
 
